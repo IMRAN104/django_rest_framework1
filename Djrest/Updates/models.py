7bb22f5862deba4865592fe3d22e2d7dd79f5d59
imran_buet13 <omeca13@gmail.com>
2020-01-21 13:12:43 +0600

    Initial Commit

---
+++ Djrest/Updates/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+
+
+def upload_update_image(instance, filename):
+    return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
+
+# Create your models here.
+
+
+class DUpdate(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=null)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to = upload_update_image, blank = True, null = True)
+    updated = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content or ""
