5ab5376e845164eaad2abd9a8370022bcd637d5c
IMRAN104 <1305104.se@ugrad.cse.buet.ac.bd>
2020-02-10 01:09:46 +0600

    Updated

--- Blog/urls.py
+++ Blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_post_details_page, name='blog_post_details'),
+    path('<int:post_id>/', views.blog_post_details_page, name='blog_post_details'),
 ]
