6ad8e4f020351bac86d04f84635d15687bae290c
Imran <1305104.se@ugrad.cse.buet.ac.bd>
2020-02-17 15:04:00 +0600

    Before COntinuing

--- Blog/views.py
+++ Blog/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
-
-from .models import BlogPost
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 # Create your views here.
+from .models import BlogPost
 
 
 def blog_post_details_page(request, post_id):
-    blog_obj = BlogPost.objects.get(id=post_id)
+    print("Class of post_id: " + post_id.__class__)
+
+    blog_obj = get_object_or_404(BlogPost, id=post_id)
+    #Broad Way of doing the same as above line
+    # try:
+    #     blog_obj = BlogPost.objects.get(id=post_id)
+    # except BlogPost.DoesNotExist:
+    #     raise Http404
+    # except ValueError:
+    #     raise Http404
+
     template_name = 'Blog/blog_post_details.html'
     context = {
         'blog_post': blog_obj
