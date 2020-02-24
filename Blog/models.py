1c0c6f94460c3b72d9f0d905f98c6ee15dab71f1
imran_buet13 <omeca13@gmail.com>
2020-02-24 15:21:04 +0600

    Before ontinueing

--- Blog/models.py
+++ Blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class BlogPost(models.Model):
     title = models.TextField(max_length = 50)
-    slug = models.SlugField(default='hello')
+    slug = models.SlugField(default='hello',max_length=30)
     content = models.TextField(null = True, blank = True)
 
     def __str__(self):
